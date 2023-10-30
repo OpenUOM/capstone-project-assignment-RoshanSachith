36e4097ca911f94fc2a8cd5e3f04299e72fcb0fc
RoshanSachith <tmsroshan23@gmail.com>
2023-10-30 14:50:21 +0530

    file changes

--- backend/seeds/dummyData.js
+++ backend/seeds/dummyData.js
@@ -1,13 +1,14 @@
-exports.seed = function(knex) {
-  return knex('dummyData').del()
+exports.seed = function (knex) {
+  return knex("dummyData")
+    .del()
     .then(function () {
-      return knex('dummyData').insert([
-        { id: 0000001, dummyDataOne: 'DummyData', dummyDataOne: 'DummyData'},
-        { id: 0000002, dummyDataOne: 'DummyData', dummyDataOne: 'DummyData'},
-        { id: 0000003, dummyDataOne: 'DummyData', dummyDataOne: 'DummyData'},
-        { id: 0000004, dummyDataOne: 'DummyData', dummyDataOne: 'DummyData'},
-        { id: 0000005, dummyDataOne: 'DummyData', dummyDataOne: 'DummyData'},
-        { id: 0000006, dummyDataOne: 'DummyData', dummyDataOne: 'DummyData'},
+      return knex("dummyData").insert([
+        { id: 0000001, dummyDataOne: "DummyData", dummyDataOne: "DummyData" },
+        { id: 0000002, dummyDataOne: "DummyData", dummyDataOne: "DummyData" },
+        { id: 0000003, dummyDataOne: "DummyData", dummyDataOne: "DummyData" },
+        { id: 0000004, dummyDataOne: "DummyData", dummyDataOne: "DummyData" },
+        { id: 0000005, dummyDataOne: "DummyData", dummyDataOne: "DummyData" },
+        { id: 0000006, dummyDataOne: "DummyData", dummyDataOne: "DummyData" },
       ]);
     });
 };
